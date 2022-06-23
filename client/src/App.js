63bb048c991d2a021c5847471f76c19449574365
Nicholas_Huang713 <nhuang713@gmail.com>
2022-06-22 21:30:52 -0700

    update title

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pokemon Cards App</h1>
+        <h1>Pokemon Cards App Hello</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
