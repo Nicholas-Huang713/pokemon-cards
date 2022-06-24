7797a8f0b5013a59f00deae54cad5bce81fa23de
Nicholas_Huang713 <nhuang713@gmail.com>
2022-06-23 22:54:11 -0700

    title text

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pokemon Cards App Hello World !!!</h1>
+        <h1>Pokemon Cards App Hello World </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
